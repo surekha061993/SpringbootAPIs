9f866824ed7a7f1c1a15011baa6fa8bb14e02c55
user <user@DESKTOP-OUCT91H>
2020-08-24 09:10:38 -0800

    create security configuration file

--- SpringbootApis/src/main/java/com/example/demo/model/Employee.java
+++ SpringbootApis/src/main/java/com/example/demo/model/Employee.java
@@ -2,4 +2,74 @@ package com.example.demo.model;
 
 public class Employee {
 
+	private int id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String gender;
+	
+	private String mobile;
+
+	
+	
+	public Employee(int id, String name, String address, String gender, String mobile) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.gender = gender;
+		this.mobile = mobile;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + ", gender=" + gender + ", mobile="
+				+ mobile + "]";
+	}
+	
+	
+	
+	
 }
